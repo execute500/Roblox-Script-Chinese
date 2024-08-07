23a16a271987a20e8b77476e2419d12c6a292d99
execute500 <166642856+execute500@users.noreply.github.com>
2024-08-07 23:47:13 +0800

    Add files via upload

---
+++ roblox/通用/偷别人物品栏道具.lua
@@ -0,0 +1,7 @@
+--Copy And Paste:
+for i,v in pairs (game.Players:GetChildren()) do
+wait()
+   for i,b in pairs (v.Backpack:GetChildren()) do
+b.Parent = game.Players.LocalPlayer.Backpack
+   end
+end
